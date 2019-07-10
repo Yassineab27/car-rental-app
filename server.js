024e1ac100e526b6204f1d8bdc58daf4f41534c1
Yassineab27 <45517237+Yassineab27@users.noreply.github.com>
2019-07-10 13:54:21 +0100

    adding rentalRoute and connecting mongodb

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require("./src/app");
 
 const server = http.createServer(app);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`Listening to the ports ${port}`));
--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 const express = require("express");
 
+const connectDB = require("../config/db");
+
 const app = express();
 
-app.use("/", (req, res) => {
-  res.send("Hellooooo");
-});
+// Connecting MONGODB
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const rentalRouter = require("./routes/rentalRoutes");
+
+app.use("/rentals", rentalRouter);
 
 module.exports = app;
